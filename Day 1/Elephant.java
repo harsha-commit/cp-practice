8034de1926b291da488be03376d7fe933723dbe6
harsha-commit <harshavardhan.bashavathini@gmail.com>
2023-06-28 19:48:52 +0530

    Solved Elephant - Codeforces

--- Day 1/Elephant.java
+++ Day 1/Elephant.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Elephant {
-    
+
+    public static int minimumStepsToHome(int target) {
+        int steps = 0;
+        while (target > 0) {
+            steps += 1;
+            // base case
+            if (target <= 5) {
+                break;
+            }
+            // Take maximum step, to reach fast
+            target = target - 5;
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        System.out.println(minimumStepsToHome(x));
+    }
 }
